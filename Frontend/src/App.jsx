c15d80dc4e554c1e0bbf2111934b6bbb3f9c2da4
AnzarAliKhan <khananzarali7@gmail.com>
2026-07-07 11:10:11 +0530

    Database Connected

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { Route, Link, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Writings from "./components/Writings";
 function App(){
   return(
     <>
     <nav>
       <Link to="/">Home</Link>
+      <Link to="/Writings">Writings</Link>
     </nav>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
+      <Route path="/Writings" element={<Writings/>}></Route>
     </Routes>
     </>
   )
